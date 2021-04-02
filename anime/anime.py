92ecde58452876f6408d40e428a8d9e1a388d5db
elf <49889930+elfq@users.noreply.github.com>
2021-04-02 03:16:47 -0400

    Update anime.py

--- anime/anime.py
+++ anime/anime.py
@@ -3,33 +3,102 @@ from discord.ext import commands
 
 import aiohttp
 
+
 class AnimePlugin(commands.Cog):
-  """A plugin to browse through various animes and find stats about them!"""
+    """A plugin for various anime commands!"""
 
-  def __init__(self, bot: commands.Bot):
-    self.bot = bot
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
 
-  @commands.command(name = "anime")
-  async def anime_(self, ctx : commands.Context, *, anime):
-    async with aiohttp.ClientSession() as cs:
-        async with cs.get(f"https://api.jikan.moe/v3/search/anime?q={anime}") as r:
-            re = await r.json()
-            embed = discord.Embed(
-                description=re["results"][0]["synopsis"], color=0x7289DA
-            )
-            embed.set_author(
-                name=re["results"][0]["title"], icon_url=ctx.author.avatar_url
-            )
-            embed.add_field(
-                name="Information",
-                value=f"**Episodes**: {re['results'][0]['episodes']}\n**Age Rating**: {re['results'][0]['rated']}\n**Start Date**: {re['results'][0]['start_date'][:-15]}\n**End Date**: {re['results'][0]['end_date'][:-15]}\n**Type:** {re['results'][0]['type']}",
-            )
-            embed.set_thumbnail(url=re["results"][0]["image_url"])
-            embed.set_footer(
-                text=f"⭐ score: {re['results'][0]['score']} | {re['results'][0]['members']} viewers"
-            )
-            await ctx.send(embed=embed)
+        @commands.command()
+        async def hug(self, ctx: commands.Context, user: discord.Member):
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get("https://shiro.gg/api/images/hug") as r:
+                    re = await r.json()
+                    if user == ctx.author:
+                        embed = discord.Embed(
+                            description=f"**{ctx.author.name}** hugs themselves!",
+                            color=discord.Colour.random(),
+                        )
+                        embed.set_image(url=re["url"])
+                        await ctx.send(embed=embed)
+                    else:
+                        embed = discord.Embed(
+                            description=f"**{ctx.author.name}** hugs **{user.name}**",
+                            color=discord.Colour.random(),
+                        )
+                        embed.set_image(url=re["url"])
+                        await ctx.send(embed=embed)
+
+        @commands.command()
+        async def pat(self, ctx: commands.Context, user: discord.Member):
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get("https://shiro.gg/api/images/pat") as r:
+                    re = await r.json()
+                    if user == ctx.author:
+                        embed = discord.Embed(
+                            description=f"**{ctx.author.name}** gives themselves a pat on the back!",
+                            color=discord.Colour.random(),
+                        )
+                        embed.set_image(url=re["url"])
+                        await ctx.send(embed=embed)
+                    else:
+                        embed = discord.Embed(
+                            description=f"**{ctx.author.name}** pats **{user.name}**",
+                            color=discord.Colour.random(),
+                        )
+                        embed.set_image(url=re["url"])
+                        await ctx.send(embed=embed)
+
+        @commands.group(invoke_without_command=True)
+        async def anime(self, ctx: commands.Context):
+            await ctx.send("_ _")
+
+        @anime.command()
+        async def search(self, ctx: commands.Context, *, name):
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get(
+                    f"https://api.jikan.moe/v3/search/anime?q={name}"
+                ) as r:
+                    re = await r.json()
+                    embed = discord.Embed(
+                        description=re["results"][0]["synopsis"], color=0x7289DA
+                    )
+                    embed.set_author(
+                        name=re["results"][0]["title"], icon_url=ctx.author.avatar_url
+                    )
+                    embed.add_field(
+                        name="Information",
+                        value=f"**Episodes**: {re['results'][0]['episodes']}\n**Age Rating**: {re['results'][0]['rated']}\n**Start Date**: {re['results'][0]['start_date'][:-15]}\n**End Date**: {re['results'][0]['end_date'][:-15]}\n**Type:** {re['results'][0]['type']}",
+                    )
+                    embed.set_thumbnail(url=re["results"][0]["image_url"])
+                    embed.set_footer(
+                        text=f"⭐ score: {re['results'][0]['score']} | {re['results'][0]['members']} viewers"
+                    )
+                    await ctx.send(embed=embed)
+
+        @anime.command()
+        async def image(self, ctx: commands.Context):
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get("https://shiro.gg/api/images/avatars") as r:
+                    re = await r.json()
+                    embed = discord.Embed(
+                        description=f"**Here's your anime image!**", color=0x7289DA
+                    )
+                    embed.set_image(url=re["url"])
+                    await ctx.send(embed=embed)
+
+        @anime.command(aliases=["background"])
+        async def wallpaper(self, ctx: commands.Context):
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get("https://shiro.gg/api/images/wallpapers") as r:
+                    re = await r.json()
+                    embed = discord.Embed(
+                        description=f"**Here's your background!**", color=0x7289DA
+                    )
+                    embed.set_image(url=re["url"])
+                    await ctx.send(embed=embed)
 
 
 def setup(bot: commands.Bot):
-  bot.add_cog(AnimePlugin(bot))
+    bot.add_cog(AnimePlugin(bot))
